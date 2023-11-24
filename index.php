799b546dfdac0cabc6d919abb1f3a4e8e4abe267
Emil <emils.boreiko@gmail.com>
2023-11-24 22:30:14 +0200

    add curl

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://google.com/search?q=coding+passive+income");
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
+?>
